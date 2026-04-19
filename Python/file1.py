9b3e42fad49005ffd98f5c04cc63ad5e6f0d5060
Jack Morris <jackowenmorris@gmail.com>
2026-04-19 10:12:30 +0100

    Made feature branch change

--- Python/file1.py
+++ Python/file1.py
@@ -1,1 +1,4 @@
 This is an example of a Python file named `file1.py`. You can add your code here to perform various tasks. For instance, you might want to define a function, create a class, or write some logic to process data. Here's a simple example of a function that prints "Hello, World!":
+def greet():    print("Hello, World!")
+# Call the function to see the outputgreet()You can replace the content of the `greet` function with any code you want to execute. For example, if you want to perform a calculation, you could modify the function like this:
+greet() 
